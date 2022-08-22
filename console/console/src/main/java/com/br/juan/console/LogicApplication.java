7452f8e161b099f8b1e51378e3f16bc67616be50
jsuisjuan <juanc.s.delima@gmail.com>
2022-08-22 11:35:50 -0300

    first commit

---
+++ console/console/src/main/java/com/br/juan/console/LogicApplication.java
@@ -0,0 +1,14 @@
+package com.br.juan.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicApplication.class, args);
+		System.out.println("Bem vindo ao java console");
+	}
+
+}
