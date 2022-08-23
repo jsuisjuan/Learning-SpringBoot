8eb2ead12548ac24a3c0247804dee117beb9801c
jsuisjuan <juanc.s.delima@gmail.com>
2022-08-23 09:29:33 -0300

    automatizacao do start.sh

--- console/console/src/main/java/com/br/juan/console/LogicApplication.java
+++ console/console/src/main/java/com/br/juan/console/LogicApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogicApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LogicApplication.class, args);
 		System.out.println("Bem vindo ao java console");
